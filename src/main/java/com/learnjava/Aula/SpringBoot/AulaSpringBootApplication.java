e72256cef60f6341721e531457e3cb152b06d6e1
kel2203 <raquelsb2001@gmail.com>
2023-04-28 16:24:50 -0300

    commit

--- src/main/java/com/learnjava/Aula/SpringBoot/AulaSpringBootApplication.java
+++ src/main/java/com/learnjava/Aula/SpringBoot/AulaSpringBootApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.learnjava"})
 @SpringBootApplication
 public class AulaSpringBootApplication implements CommandLineRunner {
-//teste
+
 	@Autowired
  private SalaryService salaryService;
 
